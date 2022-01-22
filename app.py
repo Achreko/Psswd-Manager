efb4fed37806c82acc41814daf208aabb494c22d
Achreko <achrekv3@gmail.com>
2022-01-22 01:20:30 +0100

    Refactoring. Flash messages added. Strong password

--- app.py
+++ app.py
@@ -1,25 +1,67 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,redirect, flash
 from db_storage import *
+from flask_bcrypt import Bcrypt
+from flask_login import login_user, LoginManager, login_required, logout_user
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'test'
 
+
+bcrypt = Bcrypt()
 db.init_app(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
     return render_template('startpage.html')
 
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hash = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password = hash)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    else:
+        flash("Username already taken.")
+
     return render_template('register.html', form = form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form = form)
 
 if __name__ == '__main__':
--- db_storage.py
+++ db_storage.py
@@ -1,20 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+import re
 
 db = SQLAlchemy()
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-
 class RegistrationForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min = 5, max=20)], render_kw={"placeholder": "Username"})
-    password = StringField(validators=[InputRequired(), Length(min = 5, max=20)], render_kw={"placeholder": "Password"})
+    username = StringField(validators=[InputRequired(), Length(min = 7, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min = 7, max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField("Register")
 
@@ -23,11 +25,17 @@ class RegistrationForm(FlaskForm):
             username = username.data).first()
 
         if exitsting_users:
-            raise ValidationError("This username is already taken. Please choose a different one.")
-        
+            raise ValidationError(
+                "This username is already taken. Please choose a different one.")
+    
+    def validate_password(self, password):
+        if  not password.data.isalnum():
+            raise ValidationError(
+                "Password has to contain at least 1 digit, uppercase letter and nonalphanumeric character.")
+  
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min = 5, max=20)], render_kw={"placeholder": "Username"})
-    password = StringField(validators=[InputRequired(), Length(min = 5, max=20)], render_kw={"placeholder": "Password"})
+    password = PasswordField(validators=[InputRequired(), Length(min = 5, max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField("Login")
